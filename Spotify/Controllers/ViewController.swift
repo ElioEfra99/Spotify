357fe110ce67c56b193d02a4240e55f50ed93b8b
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2022-06-11 16:40:10 -0500

    Initial Commit

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Eliu Efraín Díaz Bravo on 11/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
